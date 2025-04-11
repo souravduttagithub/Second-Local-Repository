bdfb8437974a6b93cfc536d4a0b416dd01c5a85d
souravduttagithub <duttasourav7586@gmail.com>
2025-04-11 08:55:37 +0530

    Add a new line

--- FastCprogrammingfile.c
+++ FastCprogrammingfile.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 int main{
-  printf("Hello World")
-  printf("This is the second line")
+  printf("Hello World");
+  printf("This is the second line");
+  printf("This is the third line");
 }
